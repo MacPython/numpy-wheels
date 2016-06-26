331a5a843ea25bde1f695a26a53dbdf6513f1b55
Matthew Brett <matthew.brett@gmail.com>
2016-06-26 03:28:21 -0700

    BF: Fix test return values
    Tests were not passing error codes back, so failing tests runs were
    marked as passed.

--- config.sh
+++ config.sh
@@ -11,11 +11,16 @@ function pre_build {
 
 function run_tests {
     # Runs tests on installed distribution from an empty directory
+    test_cmd="import sys; import numpy; \
+        sys.exit(not numpy.test('full').wasSuccessful())"
     if [ -n "$IS_OSX" ]; then  # Test both architectures on OSX
-        arch -i386 python -c 'import numpy; numpy.test("full")'
-        arch -x86_64 python -c 'import numpy; numpy.test("full")'
+        # Skip f2py tests for 32-bit
+        arch -i386 python -c "import sys; import numpy; \
+            sys.exit(not numpy.test('full', \
+            extra_argv=['-e', 'f2py']).wasSuccessful())"
+        arch -x86_64 python -c "$test_cmd"
     else
-        python -c 'import numpy; numpy.test("full")'
+        python -c "$test_cmd"
     fi
     # Show BLAS / LAPACK used
     python -c 'import numpy; numpy.show_config()'
