c9be88b0d0fa99922652a73655f6c8fcca91a291
Charles Harris <charlesr.harris@gmail.com>
2019-01-08 11:40:32 -0700

    testing

--- env_vars.sh
+++ env_vars.sh
@@ -1,4 +1,4 @@
 # Environment variables for build
 OPENBLAS_VERSION="v0.3.3-186-g701ea883"
 MACOSX_DEPLOYMENT_TARGET=10.9
-CFLAGS="-std=iso9899:2011 -fno-strict-aliasing"
+CFLAGS="-std=c99 -fno-strict-aliasing"
