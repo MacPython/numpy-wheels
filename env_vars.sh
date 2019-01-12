d7c3d69a4fba6b25b7550ac385c26fb63f56597d
Charles Harris <charlesr.harris@gmail.com>
2019-01-12 16:52:27 -0700

    TST: Use old OpenBLAS version

--- env_vars.sh
+++ env_vars.sh
@@ -1,4 +1,5 @@
 # Environment variables for build
-OPENBLAS_VERSION="v0.3.3-186-g701ea883"
+#OPENBLAS_VERSION="v0.3.3-186-g701ea883"
+OPENBLAS_VERSION="v0.3.0"
 MACOSX_DEPLOYMENT_TARGET=10.9
 CFLAGS="-std=c99 -fno-strict-aliasing"
