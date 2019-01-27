54bb7f68c32e78ecdc609d1553d88a3c298d541f
Charles Harris <charlesr.harris@gmail.com>
2019-01-27 06:54:43 -0700

    testing

--- env_vars.sh
+++ env_vars.sh
@@ -1,4 +1,8 @@
 # Environment variables for build
 OPENBLAS_VERSION="v0.3.3-186-g701ea883"
 MACOSX_DEPLOYMENT_TARGET=10.9
-CFLAGS="-std=c99 -O2 -fno-strict-aliasing"
+if [ "$PLAT"  == "i686" ]; then
+    CFLAGS="-std=c99 -O2 -fno-strict-aliasing"
+else
+    CFLAGS="-std=c99 -fno-strict-aliasing"
+fi
