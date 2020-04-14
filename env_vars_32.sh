3cf8f1e176803ff26ec2018f9c88b65fdd253e70
mattip <matti.picus@gmail.com>
2020-04-14 12:32:18 +0300

    use manylinux2010, fix order of commands on windows

--- env_vars_32.sh
+++ env_vars_32.sh
@@ -1,6 +1,7 @@
 # Environment variables for 32-bit build.
 # The important difference from the 64-bit build is `-msse2` to
 # compile sse loops for ufuncs.
+set -x
 OPENBLAS_VERSION="v0.3.7"
 MACOSX_DEPLOYMENT_TARGET=10.9
 CFLAGS="-msse2 -std=c99 -fno-strict-aliasing"
