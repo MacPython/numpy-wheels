a5d15431368bd146b66134ddfeb3d2ecd78b80a4
mattip <matti.picus@gmail.com>
2020-05-01 00:46:47 +0300

    prepend to CFLAGS rather than overrid

--- env_vars_32.sh
+++ env_vars_32.sh
@@ -6,4 +6,4 @@ OPENBLAS_VERSION="v0.3.7"
 MACOSX_DEPLOYMENT_TARGET=10.9
 # Causes failure for pre-1.19 in np.reciprocal
 # CFLAGS="-msse2 -std=c99 -fno-strict-aliasing"
-CFLAGS="-std=c99 -fno-strict-aliasing"
+CFLAGS="-std=c99 -fno-strict-aliasing $CFLAGS"
