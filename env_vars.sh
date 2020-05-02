dcceacd42a2346d9395511e64ad93a7a4b89f79a
mattip <matti.picus@gmail.com>
2020-05-02 21:29:18 +0300

    use --strip-debug

--- env_vars.sh
+++ env_vars.sh
@@ -1,4 +1,4 @@
 # Environment variables for build
 OPENBLAS_VERSION="v0.3.7"
 MACOSX_DEPLOYMENT_TARGET=10.9
-CFLAGS="-std=c99 -fno-strict-aliasing $CFLAGS"
+CFLAGS="-std=c99 -fno-strict-aliasing -Wl,--strip-debug"
--- env_vars_32.sh
+++ env_vars_32.sh
@@ -5,5 +5,5 @@ set -x
 OPENBLAS_VERSION="v0.3.7"
 MACOSX_DEPLOYMENT_TARGET=10.9
 # Causes failure for pre-1.19 in np.reciprocal
-# CFLAGS="-msse2 -std=c99 -fno-strict-aliasing"
-CFLAGS="-std=c99 -fno-strict-aliasing $CFLAGS"
+# CFLAGS="-msse2 -std=c99 -fno-strict-aliasing -Wl,--strip-debug"
+CFLAGS="-std=c99 -fno-strict-aliasing -Wl,--strip-debug"
