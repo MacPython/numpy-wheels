83df1e90ec4cae0701d636aa95d1c6ec024e4ca9
mattip <matti.picus@gmail.com>
2020-05-06 09:34:25 +0300

    use ldflags

--- env_vars_32.sh
+++ env_vars_32.sh
@@ -5,5 +5,6 @@ set -x
 OPENBLAS_VERSION="v0.3.7"
 MACOSX_DEPLOYMENT_TARGET=10.9
 # Causes failure for pre-1.19 in np.reciprocal
-# CFLAGS="-msse2 -std=c99 -fno-strict-aliasing -Wl,--strip-debug"
-CFLAGS="-std=c99 -fno-strict-aliasing -Wl,--strip-debug"
+# CFLAGS="-msse2 -std=c99 -fno-strict-aliasing"
+CFLAGS="-std=c99 -fno-strict-aliasing"
+LDFLAGS="-Wl,--strip-debug"
