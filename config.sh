ea39e0d2f66c062abdad8738e489a866e4ac8b33
Charles Harris <charlesr.harris@gmail.com>
2021-10-19 21:06:55 -0600

    Second try at building macos 3.10 universal2 wheel

--- config.sh
+++ config.sh
@@ -19,26 +19,6 @@ function build_wheel {
     wrap_wheel_builder _build_wheel $@
 }
 
-function pyinst_fname_for_version {
-    # echo filename for OSX installer file given Python and minimum
-    # macOS versions
-    # Parameters
-    #   $py_version (Python version in major.minor.extra format)
-    #   $py_osx_ver: {major.minor | not defined}
-    #       if defined, the minimum macOS SDK version that Python is
-    #       built for, eg: "10.6" or "10.9", if not defined, infers
-    #       this from $py_version using macpython_sdk_for_version
-    local py_version=$1
-    local py_osx_ver=${2:-$(macpython_sdk_for_version $py_version)}
-    local inst_ext=$(pyinst_ext_for_version $py_version)
-    echo "python-${py_version}-macosx${py_osx_ver}.${inst_ext}"
-}
-
-function install_delocate {
-    check_pip
-    $PIP_CMD install git+https://github.com/isuruf/delocate.git@arm64
-}
-
 function build_libs {
     # setuptools v49.2.0 is broken
     $PYTHON_EXE -mpip install --upgrade "setuptools<49.2.0"
@@ -63,6 +43,7 @@ function get_test_cmd {
 
 function run_tests {
     # Runs tests on installed distribution from an empty directory
+
     if [ -n "$IS_LINUX" ]; then
         apt-get -y update && apt-get install -y gfortran
     fi
