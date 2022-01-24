e75a6a06f8a7f262578bb54d07507eff1a95434b
Matthew Brett <matthew.brett@gmail.com>
2022-01-24 19:36:42 +0000

    Respond to bitness of build.

--- azure/set_141.cmd
+++ azure/set_141.cmd
@@ -1,4 +1,9 @@
 REM set Visual Studio 2017 toolchain (v141)
-REM Needs to alter environment
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat" -vcvars_ver=14.16
+IF [%1] == [] GOTO NoArgErr
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars%1.bat" -vcvars_ver=14.16
 set DISTUTILS_USE_SDK=1
+goto:eof
+
+:NoArgErr
+    echo "No input bitness argument"
+    exit 1
