75005fb9df38dab78f958e90931bba72fec8ca97
Matthew Brett <matthew.brett@gmail.com>
2022-01-24 17:43:53 +0000

    Use 141 toolchain for Windows build
    See discussion at https://github.com/MacPython/numpy-wheels/issues/145

---
+++ azure/set_141.cmd
@@ -0,0 +1,4 @@
+REM set Visual Studio 2017 toolchain (v141)
+REM Needs to alter environment
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat" -vcvars_ver=14.16
+set DISTUTILS_USE_SDK=1
