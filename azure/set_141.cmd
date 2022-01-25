adacbd5a0f3ec7d16558054daeaeaf6f7d02dd12
Matthew Brett <matthew.brett@gmail.com>
2022-01-25 00:33:32 +0000

    Use conda-forge activate.bat tricks
    From
    https://github.com/conda-forge/vc-feedstock/blob/7c0aa76218f369227d6a7fc78f981b100d68d50a/recipe/activate.bat
    with thanks.

--- azure/set_141.cmd
+++ azure/set_141.cmd
@@ -1,9 +1,56 @@
+@@echo on
 REM set Visual Studio 2017 toolchain (v141)
 IF [%1] == [] GOTO NoArgErr
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars%1.bat" -vcvars_ver=14.16
+set VS_BITS=%1
+IF [%2] == [] GOTO NoArgErr
+set VS_YEAR=%2
+
+
+if not exist "%VSINSTALLDIR%" (
+set "VSINSTALLDIR=%ProgramFiles(x86)%\Microsoft Visual Studio\%VS_YEAR%\Professional\"
+)
+if not exist "%VSINSTALLDIR%" (
+set "VSINSTALLDIR=%ProgramFiles(x86)%\Microsoft Visual Studio\%VS_YEAR%\Community\"
+)
+if not exist "%VSINSTALLDIR%" (
+set "VSINSTALLDIR=%ProgramFiles(x86)%\Microsoft Visual Studio\%VS_YEAR%\BuildTools\"
+)
+if not exist "%VSINSTALLDIR%" (
+set "VSINSTALLDIR=%ProgramFiles(x86)%\Microsoft Visual Studio\%VS_YEAR%\Enterprise\"
+)
+
+call :GetWin10SdkDir
+:: dir /ON here is sorting the list of folders, such that we use the latest one that we have
+for /F %%i in ('dir /ON /B "%WindowsSdkDir%\include\10.*"') DO (
+  SET WindowsSDKVer=%%~i
+)
+if errorlevel 1 (
+    echo "Didn't find any windows 10 SDK. I'm not sure if things will work, but let's try..."
+) else (
+    echo Windows SDK version found as: "%WindowsSDKVer%"
+)
+
+call "%VSINSTALLDIR%\VC\Auxiliary\Build\vcvars%1.bat" -vcvars_ver=14.16 %WindowsSDKVer%
 set DISTUTILS_USE_SDK=1
 goto:eof
 
+:GetWin10SdkDir
+call :GetWin10SdkDirHelper HKLM\SOFTWARE\Wow6432Node > nul 2>&1
+if errorlevel 1 call :GetWin10SdkDirHelper HKCU\SOFTWARE\Wow6432Node > nul 2>&1
+if errorlevel 1 call :GetWin10SdkDirHelper HKLM\SOFTWARE > nul 2>&1
+if errorlevel 1 call :GetWin10SdkDirHelper HKCU\SOFTWARE > nul 2>&1
+if errorlevel 1 exit /B 1
+exit /B 0
+
+:GetWin10SdkDirHelper
+@@REM `Get Windows 10 SDK installed folder`
+for /F "tokens=1,2*" %%i in ('reg query "%1\Microsoft\Microsoft SDKs\Windows\v10.0" /v "InstallationFolder"') DO (
+    if "%%i"=="InstallationFolder" (
+        SET WindowsSdkDir=%%~k
+    )
+)
+exit /B 0
+
 :NoArgErr
-    echo "No input bitness argument"
+    echo "Need to specify input bitness and VS year"
     exit 1
